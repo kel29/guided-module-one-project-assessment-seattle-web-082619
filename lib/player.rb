3e3566ac507289aeeae3591a3f3224c14b5ebd92
kel29 <krista.loven.87@gmail.com>
2019-09-11 09:45:18 -0700

    fix play card validation and implement wild card

--- lib/player.rb
+++ lib/player.rb
@@ -22,52 +22,41 @@ class Player < ActiveRecord::Base
     def play_card(deck_id)
         puts "Enter the play code of the card you would like to play: "
         card_code = STDIN.gets.strip.upcase
-        card_not_in_hand_error_loop(deck_id, card_code)
-        validate_card_is_playable(deck_id, card_code)
-        move_card_from_hand_to_pile(deck_id, card_code)
-        puts "You've successfully played a card."
+        top = find_top_card(deck_id)[0]
+        play_card = find_card_in_hand(deck_id, card_code)
+        if play_card.nil?
+            puts
+            puts "Looks like that is not a valid card code or "
+            puts "that card is not in your hand. "
+        elsif play_card.value == "8"
+            puts "Eights are wild! What suit would you like to switch to?"
+            suit = STDIN.gets.strip.upcase
+            until suit == "HEARTS" || suit == "SPADES" || suit == "CLUBS" || suit == "DIAMONDS"
+                puts "Mmm, doesn't look like you entered a valid suit. "
+                puts "Please [enter] 'spades', 'clubs', 'hearts', or 'diamonds'."
+                suit = STDIN.gets.strip.upcase
+            end
+            move_card_from_hand_to_pile(deck_id, card_code)
+            Hand.forget_top_card(deck_id)
+            Hand.create(location: "top", deck_id: deck_id, suit: suit)
+        elsif top.suit == play_card.suit || top.value == play_card.value
+            move_card_from_hand_to_pile(deck_id, card_code)
+            puts "You've successfully played a card."
+        else
+            puts "Looks like that's not a valid card to play."
+            puts "Remember that either the card number or suit needs "
+            puts "to match the card on the top of the discard pile. "
+        end
     end
 
     def find_card_in_hand(deck_id, card_code)
         player_hand(deck_id).find { |c| c.code == card_code }
     end
 
-    def card_not_in_hand_error_loop(deck_id, card_code)
-        until find_card_in_hand(deck_id, card_code)
-            puts
-            puts "Looks like that is not a valid card code or that card is "
-            puts "not in your hand. As a quick refresher, here is your hand:"
-            view_hand(deck_id)
-            puts
-            puts "Select a card to play by entering their play code:"
-            card_code = STDIN.gets.strip.upcase
-        end
-    end
-
-    def validate_card_is_playable(deck_id, card_code) #doesn't work if someone loops too many times
-        top = find_top_card(deck_id)[0]
-        play_card = find_card_in_hand(deck_id, card_code)
-        # binding.pry
-        until top.suit == play_card.suit || top.value == play_card.value || play_card.value == "8"
-            puts "Looks like that's not a valid card to play."
-            # puts "First, you can only play cards that are in you hand. "
-            # view_hand(deck_id)
-            puts "Then, remember that either the card number or suit needs "
-            puts "to match the card on the top of the discard pile. "
-            view_top_card(deck_id)
-            puts "Please enter a valid card code: "
-            card_code = STDIN.gets.strip.upcase
-            # binding.pry
-            card_not_in_hand_error_loop(deck_id, card_code)
-            # binding.pry
-            play_card = find_card_in_hand(deck_id, card_code)
-        end
-    end
-
     def move_card_from_hand_to_pile(deck_id, card_code)
         Hand.forget_top_card(deck_id)
         new_top = find_card_in_hand(deck_id, card_code)
-        new_top.location = "top"
+        new_top[:location] = "top"
         new_top.save
     end
 
