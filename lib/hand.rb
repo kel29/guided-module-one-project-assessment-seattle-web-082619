a1a132c741b871645be7abce2aa47764683ebc1d
Krista Loven <krista.loven.87@gmail.com>
2019-09-17 11:39:39 -0700

    moved to models file

--- lib/hand.rb
+++
@@ -1,9 +0,0 @@
-class Hand < ActiveRecord::Base 
-
-    def self.forget_top_card(deck_api_id)
-        old = self.find_by("location = ? AND deck_api_id = ?", "top", deck_api_id)
-        old.location = "discard"
-        old.save
-    end
-
-end
